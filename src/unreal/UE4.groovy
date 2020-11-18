b86b20a28788695c1d4b8ef0f5db629bfb6b0497
Shentoza <wuelk@hotmail.de>
2020-11-18 13:55:29 +0100

    capitalize Get functions

--- src/unreal/UE4.groovy
+++ src/unreal/UE4.groovy
@@ -11,12 +11,12 @@ def RemoveOldBuilds() {
 }
 
 def GenerateProjectfiles() {
-    bat getEngineFolder() + '/Binaries/DotNET/UnrealBuildTool.exe -projectfiles -project=' + getUDFolder() + '/UpsideDrown.uproject -game -progress'
+    bat GetEngineFolder() + '/Binaries/DotNET/UnrealBuildTool.exe -projectfiles -project=' + GetUDFolder() + '/UpsideDrown.uproject -game -progress'
 }
 
 def buildEditor( String platform ) {
     stage ( 'Build Editor Win64 for ' + platform ) {
-        bat getEngineFolder() + "/Binaries/DotNET/UnrealBuildTool.exe ${env.PROJECT_NAME}Editor Win64 Development " + getUDFolder() + "/${env.PROJECT_NAME}.uproject"
+        bat GetEngineFolder() + "/Binaries/DotNET/UnrealBuildTool.exe ${env.PROJECT_NAME}Editor Win64 Development " + GetUDFolder() + "/${env.PROJECT_NAME}.uproject"
     }
 }
 
@@ -40,14 +40,14 @@ def buildCookRun( String platform, String buildConfig ) {
     can_archive_project = can_archive_project && Storeflag
 
     stage ( 'Build ' + platform ) {
-        bat getUATCommonArguments( platform, buildConfig ) + getUATBuildArguments()
+        bat GetUATCommonArguments( platform, buildConfig ) + GetUATBuildArguments()
     }
 
     if ( can_cook_project ) {
         stage ( 'Cook ' + platform ) {
             // Some platforms may need specific commands to be executed before the cooker starts
             executePlatformPreCookCommands( platform )
-            bat getUATCommonArguments( platform, buildConfig ) + getUATCookArguments( platform, buildConfig, can_archive_project )
+            bat GetUATCommonArguments( platform, buildConfig ) + GetUATCookArguments( platform, buildConfig, can_archive_project )
             executePlatformPostCookCommands( platform )
         }
     }
@@ -58,46 +58,46 @@ def archiveBuild(String platform, String buildConfig) {
 	Boolean ShouldArchiveShipping = (buildConfig == 'All' || buildConfig == 'Shipping')
 	echo "ShouldArchiveShipping: ${ShouldArchiveShipping}"
 	if(ShouldArchiveShipping) {
-		bat '''"%SevenZipPath%/7z.exe"'''+" a -t7z "+getOutputDirectory(platform, 'Shipping')+"/"+getArchiveName(platform, 'Shipping')+ " " +getOutputDirectory(platform, 'Shipping')+"/."
+		bat '''"%SevenZipPath%/7z.exe"'''+" a -t7z "+GetOutputDirectory(platform, 'Shipping')+"/"+GetArchiveName(platform, 'Shipping')+ " " +GetOutputDirectory(platform, 'Shipping')+"/."
 	}
 	Boolean ShouldArchiveDevelopment = buildConfig == 'All' || buildConfig == 'Development'
 	echo "ShouldArchiveDevelopment: ${ShouldArchiveDevelopment}"
 	if(ShouldArchiveDevelopment) {
-		bat '''"%SevenZipPath%/7z.exe"'''+" a -t7z "+getOutputDirectory(platform, 'Development')+"/"+getArchiveName(platform, 'Development')+ " " +getOutputDirectory(platform, 'Development')+"/."
+		bat '''"%SevenZipPath%/7z.exe"'''+" a -t7z "+GetOutputDirectory(platform, 'Development')+"/"+GetArchiveName(platform, 'Development')+ " " +GetOutputDirectory(platform, 'Development')+"/."
 	}
 }
 
 def ArtifactLogs() {
-    bat '''"%SevenZipPath%/7z.exe"'''+" a -t7z "+workspace+"/Temp/Logs.7z"+" " + getEngineFolder()+"/Programs/AutomationTool/Saved/."
+    bat '''"%SevenZipPath%/7z.exe"'''+" a -t7z "+workspace+"/Temp/Logs.7z"+" " + GetEngineFolder()+"/Programs/AutomationTool/Saved/."
     archiveArtifacts allowEmptyArchive: true, artifacts: 'Temp/**/*.7z', caseSensitive: false, fingerprint: true
 }
 
-def getArchiveName(String platform, String buildConfig) {
+def GetArchiveName(String platform, String buildConfig) {
     return "${P4STREAMNAME}/${env.VERSION_STRING}-${Platform}-${buildConfig}.7z"
 }
 
 //Full outputh path
-def getOutputDirectory( String platform, String buildConfig ) {
-    return workspace +'/'+ getOutputDirFromProjectRoot(platform, buildConfig)
+def GetOutputDirectory( String platform, String buildConfig ) {
+    return workspace +'/'+ GetOutputDirFromProjectRoot(platform, buildConfig)
 }
 
 //relative to Project folder path, so we can use it for artifacts
-def getOutputDirFromProjectRoot( String platform, String buildConfig ) {
+def GetOutputDirFromProjectRoot( String platform, String buildConfig ) {
     return 'Temp/' + buildConfig + "/"+platform
 }
 
-def getEngineFolder() {
+def GetEngineFolder() {
     return workspace + '/Engine'
 }
 
-def getUDFolder() {
+def GetUDFolder() {
     return workspace + '/UD'
 }
 
-def getUATCommonArguments( String platform, String buildConfig ) {
-    String result = getEngineFolder() + '/Build/BatchFiles/RunUAT.bat BuildCookRun -project=' + getUDFolder() + "/${env.PROJECT_NAME}.uproject -utf8output -noP4 -platform=" + platform + ' -clientconfig=' + buildConfig
+def GetUATCommonArguments( String platform, String buildConfig ) {
+    String result = GetEngineFolder() + '/Build/BatchFiles/RunUAT.bat BuildCookRun -project=' + GetUDFolder() + "/${env.PROJECT_NAME}.uproject -utf8output -noP4 -platform=" + platform + ' -clientconfig=' + buildConfig
 	Boolean CleanFlag = "${CLEANBUILD}" == "true" ? true : false;
-    result += getUATCompileFlags(platform)
+    result += GetUATCompileFlags(platform)
 
     if ( buildConfig == 'Shipping' || CleanFlag) {
         result += ' -clean'
@@ -106,34 +106,34 @@ def getUATCommonArguments( String platform, String buildConfig ) {
     return result
 }
 
-def getUATCompileFlags(String platform) {
+def GetUATCompileFlags(String platform) {
     // -nocompile because we already have the automation tools
     // -nocompileeditor because we built it before
     String result = ' -nocompile -nocompileeditor -prereqs -nodebuginfo -ue4exe='
-    result += getEngineFolder()
+    result += GetEngineFolder()
     result += "/Binaries/"+platform+"/UE4Editor-Cmd.exe"
     return result
 }
 
-def getUATBuildArguments() {
+def GetUATBuildArguments() {
     // build only. dont cook. This is done in a separate stage
     return ' -build -skipcook'
 }
 
-def getUATCookArguments( String platform, String buildConfig, Boolean archive_project ) {
+def GetUATCookArguments( String platform, String buildConfig, Boolean archive_project ) {
     String result = ' -allmaps -cook'
 
-    result += getUATCookArgumentsFromClientConfig( buildConfig)
-    result += getUATCookArgumentsForPlatform( platform )
+    result += GetUATCookArgumentsFromClientConfig( buildConfig)
+    result += GetUATCookArgumentsForPlatform( platform )
 
     if ( archive_project ) {
-        result += ' -pak -package -stage -archive -archivedirectory=' + getOutputDirectory( platform, buildConfig )
+        result += ' -pak -package -stage -archive -archivedirectory=' + GetOutputDirectory( platform, buildConfig )
     }
 
     return result
 }
 
-def getUATCookArgumentsFromClientConfig( String buildConfig ) {
+def GetUATCookArgumentsFromClientConfig( String buildConfig ) {
     // Do not cook what has already been cooked if possible
     if ( buildConfig == 'Development' ) {
         return ' -iterativecooking'
@@ -144,7 +144,7 @@ def getUATCookArgumentsFromClientConfig( String buildConfig ) {
     }
 }
 
-def getUATCookArgumentsForPlatform( String platform ) {
+def GetUATCookArgumentsForPlatform( String platform ) {
     String result = ''
 
     // See https://docs.unrealengine.com/latest/INT/Engine/Basics/Projects/Packaging/
