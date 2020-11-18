4e4abf9779819f8b0b79f95511485b08b72c07e3
Shentoza <wuelk@hotmail.de>
2020-11-18 13:50:07 +0100

    typo in p4

--- src/unreal/P4.groovy
+++ src/unreal/P4.groovy
@@ -3,7 +3,7 @@ import unreal.JenkinsBase
 
 def GetP4WS(Streamdir, Streamname) {
     P4WS = [$class: 'StreamWorkspaceImpl',
-    charset: 'none', format :charset: 'none', format: new JenkinsBase().GetJobType() + '-${NODE_NAME}-' + "${Streamname}" + '-${EXECUTOR_NUMBER}-jenkins', pinHost: false, streamName: "${Streamdir}${Streamname}"]
+    charset: 'none', charset: 'none', format: new JenkinsBase().GetJobType() + '-${NODE_NAME}-' + "${Streamname}" + '-${EXECUTOR_NUMBER}-jenkins', pinHost: false, streamName: "${Streamdir}${Streamname}"]
 }
 
 
