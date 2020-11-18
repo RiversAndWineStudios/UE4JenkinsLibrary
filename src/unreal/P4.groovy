a5ffe126fffa2a6a36aa53b44a14a5232a1d0867
Shentoza <wuelk@hotmail.de>
2020-11-18 12:43:04 +0100

    does it work with this function?

--- src/unreal/P4.groovy
+++ src/unreal/P4.groovy
@@ -2,8 +2,9 @@ package unreal
 import unreal.JenkinsBase
 
 def GetP4WS(Streamdir, Streamname) {
-    def ws = [$class: 'StreamWorkspaceImpl',
+    P4WS = [$class: 'StreamWorkspaceImpl',
     charset: 'none', format :charset: 'none', format: new JenkinsBase().GetJobType() + '-${NODE_NAME}-' + "${Streamname}" + '-${EXECUTOR_NUMBER}-jenkins', pinHost: false, streamName: "${Streamdir}${Streamname}"]
 }
+
 
 return this
